3ba92650cd42993db80fae99af6503d6701ce63b
Letlhogonolo  Chabalala <ST10333694@rcconnect.edu.za>
2025-09-09 18:26:53 +0000

    feat(ui) : add main window header and navigation

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace CMCS
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenLecturer(object sender, RoutedEventArgs e)
+        {
+            var win = new Views.LecturerView();
+            win.Owner = this;
+            win.Show();
+        }
+
+        private void OpenCoordinator(object sender, RoutedEventArgs e)
+        {
+            var win = new Views.CoordinatorView();
+            win.Owner = this;
+            win.Show();
+        }
+
+        private void OpenManager(object sender, RoutedEventArgs e)
+        {
+            var win = new Views.ManagerView();
+            win.Owner = this;
+            win.Show();
+        }
+    }
+}
